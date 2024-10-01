73d1b3f637b7244cc6edd5d3eb2d24b2b8ec400a
Sanket Gadekar <gadekarsanket31700@gmail.com>
2024-10-01 16:43:12 +0530

    Print all Fibonacci Numbers till N

--- FibonacciNumbers.java
+++ FibonacciNumbers.java
@@ -1,3 +1,8 @@
+//Question Statement:
+//1. You've to print first n fibonacci numbers.
+//2. Take as input "n", the count of fibonacci numbers to print.
+//3. Print first n fibonacci numbers.
+
 import java.util.Scanner;
 
 public class FibonacciNumbers {
