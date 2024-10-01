9d118f97c8ade1e7d6aa0e27b348f7e2a18f0266
Sanket Gadekar <gadekarsanket31700@gmail.com>
2024-10-01 15:38:30 +0530

    Is a number Prime? (t, n)

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,26 +1,24 @@
 import java.util.Scanner;
 
-public class IsANumberPrime {
-    public static void main(String args[]){
+public class PrimeNumber {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = Integer.parseInt(sc.nextLine());
-        for(int i = 1; i <= t; i++){
+        for (int i = 1; i <= t; i++) {
             int n = Integer.parseInt(sc.nextLine());
 
             int count = 0;
-            for(int div = 2; div * div <= n; div++){
-                if(n % div == 0){
+            for (int div = 2; div * div <= n; div++) {      //check only up to under-root(n) value
+                if (n % div == 0) {
                     count++;
-                    break;
+                    break;                  //reduce unwanted looping
                 }
             }
-            if(count == 0){
+            if (count == 0) {
                 System.out.println("Prime");
-            } else{
+            } else {
                 System.out.println("Not Prime");
             }
         }
-
-
     }
 }
